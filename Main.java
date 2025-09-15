a82c84652483d124a448d370b8ad1f6ea9938cd6
Houseint <gu.paulino2007@email.com>
2025-09-15 09:56:15 -0300

    Parte 1

---
+++ Main.java
@@ -0,0 +1,32 @@
+import java.sql.*;
+
+public class Main{
+    public static void main(String[]Args){
+        //Dados da conexão
+        String url = "jdbc:mysql://localhost:3306/loja";
+        String usuario = "root";
+        String senha = ""; //Senha vazia
+
+        try{
+            Connection conexao = DriverManager.getConnection(url, usuario, senha);
+            System.out.println("Conexão bem sucedida!");
+
+            String sql = "SELECT * FROM produtos";
+            Statement stmt = conexao.createStatement();
+            ResultSet resultado = stmt.executeQuery(sql);
+
+            System.out.println("Lista de Produtos: ");
+            while (resultado.next()){
+                int id = resultado.getInt("id");
+                String nome = resultado.getString("nome");
+                double preco = resultado.getDouble("preco");
+                System.out.println("ID: " + id + ", Nome: " + nome + ", Preço: " + preco);
+            }
+            resultado.close();
+            stmt.close();
+            conexao.close();
+    } catch (SQLException e){
+            System.out.println("Erro na conexão: " + e.getMessage());
+        }
+    }
+}
